04170846ab6eaf3e821e87adeeda2edde860c923
MD NAYEEB RAHMAN <nayeebrahman@gmail.com>
2026-08-19 18:25:36 +0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { LoginComponent } from './features/auth/login/login.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  // Add a wildcard route to catch typos
+  // Wildcard route to catch any 404s and send them to login
   { path: '**', redirectTo: '/login' } 
 ];
